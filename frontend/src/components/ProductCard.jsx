935c11d83bafe7264797fdd5e0f5278982670425
Anish <work.anish.004@gmail.com>
2025-03-17 15:09:05 +0530

    added delete buttun/functionality in products

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,10 @@
 import { EditIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/useProductStore";
 
 function ProductCard({ product }) {
+  const {deleteProduct} = useProductStore();
+
   return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
       {/* Product image */}
@@ -25,7 +28,8 @@ function ProductCard({ product }) {
           </Link>
 
           <button
-            className="btn btn-sm btn-error  btn-outline"
+            className="btn btn-sm btn-error btn-outline"
+            onClick={() => deleteProduct(product.id)}
           >
             <Trash2Icon className="size-4" />
           </button>
--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const BASE_URL =  "http://localhost:3000"
 
@@ -8,7 +9,6 @@ export const useProductStore = create((set, get) => ({
   products: [],
   loading: false,
   error: null,
-
 
   fetchProducts: async () => {
     set({ loading: true });
@@ -20,8 +20,24 @@ export const useProductStore = create((set, get) => ({
       else set({ error: "Something went wrong", products: [] });
     } finally {
       set({ loading: false });
+    } 
+  },
+
+  deleteProduct: async (id) => {
+    console.log("deleteProduct function called", id);
+    set({ loading: true });
+    try {
+      await axios.delete(`${BASE_URL}/api/product/${id}`);
+      set((prev) => ({ products: prev.products.filter((product) => product.id !== id) }));
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      console.log("Error in deleteProduct function", error);
+      toast.error("Something went wrong");
+    } finally {
+      set({ loading: false });
     }
   },
+
 
 
 }));
